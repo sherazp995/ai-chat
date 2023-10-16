c43dcf29db3581d6dcedd8e3e296cf56206f6420
Sheraz <sherazp995@gmail.com>
2023-10-16 14:58:42 +0500

    Downgraded openai module

--- server/services/openai.js
+++ server/services/openai.js
@@ -1,21 +1,21 @@
-const { OpenAI } = require("openai");
 
-const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+	apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(config);
 
 const runPrompt = async (prompt) => {
-  try {
-    const response = await openai.chat.completions.create({
-      engine: "text-davinci-003",
-      prompt: prompt,
-      max_tokens: 2048,
-      temperature: 0.8,
-    });
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: prompt,
+		max_tokens: 2048,
+		temperature: 0.8,
+	});
 
-    return response.choices[0].text;
-  } catch (error) {
-    console.error("OpenAI API Error:", error);
-    throw error;
-  }
+	return response.data.choices[0].text;
 };
 
 module.exports = {
